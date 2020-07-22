2ff9dd56107c918d706ebac4de0a689e44da5572
Neil Morrison <neil.morrison44@googlemail.com>
2020-07-23 00:28:27 +0100

    try making the output white not white

--- src/components/outputCanvas.js
+++ src/components/outputCanvas.js
@@ -9,7 +9,7 @@ const OutputCanvas = ({ imageData, width = 400, height = 240 }) => {
     ctx.clearRect(0, 0, width, height)
     for (let x = 0; x < width; x++) {
       for (let y = 0; y < height; y++) {
-        ctx.fillStyle = imageData?.[y]?.[x] ? "black" : "white"
+        ctx.fillStyle = imageData?.[y]?.[x] ? "black" : "rgb(225,225,225)"
         ctx.fillRect(x, y, 1, 1)
       }
     }
