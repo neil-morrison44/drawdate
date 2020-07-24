e3da17fd7682cfbdb8e154c11275dfbf1abe853e
Neil Morrison <neil.morrison44@googlemail.com>
2020-07-24 22:04:07 +0100

    tools styling

--- src/components/tools.js
+++ src/components/tools.js
@@ -4,16 +4,18 @@ const Tools = ({ onChangeTool, toolList, selectedTool }) => {
   return (
     <div className="tools">
       <h3>Tools</h3>
-      {toolList.map(({ name, Icon }) => (
-        <button
-          key={name}
-          className={`tools__tool ${selectedTool === name ? "tools__tool--selected" : ""}`}
-          onClick={() => onChangeTool(name)}
-        >
-          <Icon />
-          {name}
-        </button>
-      ))}
+      <div className="tools__choices">
+        {toolList.map(({ name, Icon }) => (
+          <button
+            key={name}
+            className={`tools__tool ${selectedTool === name ? "tools__tool--selected" : ""}`}
+            onClick={() => onChangeTool(name)}
+          >
+            <Icon />
+            {name}
+          </button>
+        ))}
+      </div>
     </div>
   )
 }
